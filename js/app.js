c78761b189342a9b476eae6c82473453a905af5b
Meraj <merajulhasan150@gmail.com>
2022-09-04 04:26:31 +0600

    4th commit js news category upload

--- js/app.js
+++ js/app.js
@@ -9,3 +9,17 @@ const loadCategories = async() =>{
     console.log(error);
   }
 }
+
+const displayCategories = (data) =>{
+    
+  const categoriesContainer = document.getElementById('categories-container');
+  data.forEach(categorie =>{
+    const {category_id, category_name} = categorie;
+    // console.log(category_id);
+     const categorieDiv = document.createElement('div');
+     categorieDiv.innerHTML = `
+     <button class="btn btn-outline-primary" onclick="loadNews(${category_id}), toggleSpinner(true)"><h5>${category_name}</h5></button>
+     `;
+     categoriesContainer.appendChild(categorieDiv);
+  })
+}
